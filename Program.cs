c53e13b4b391df74d7f8b338b0a9ba417c2a260b
Denis Konovalov <den.konovaloff@gmail.com>
2018-10-18 09:56:30 +0400

    Описание задачи

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+//Алгори́тм Де́йкстры (англ. Dijkstra’s algorithm) — алгоритм на графах
+//Находит кратчайшие пути от одной из вершин графа до всех остальных. 
 namespace Dijkstra
 {
     class Program
